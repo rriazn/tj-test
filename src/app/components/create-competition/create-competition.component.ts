2816052d5aab21fcf12c66a4c3f906558fe7de48
rriazn <rnriazanov@gmail.com>
2025-07-02 15:59:07 +0200

    input comp

--- src/app/components/create-competition/create-competition.component.ts
+++ src/app/components/create-competition/create-competition.component.ts
@@ -1,10 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CompetitionService } from '../../services/competition.service';
 import { CompetitionComponent } from '../competition/competition.component';
+import { Competition } from '../../model/competition.type';
+import { DateTime } from 'luxon';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-competition',
-  imports: [CompetitionComponent],
+  imports: [CompetitionComponent, FormsModule],
   templateUrl: './create-competition.component.html',
   styleUrl: './create-competition.component.scss'
 })
@@ -13,7 +16,30 @@ export class CreateCompetitionComponent {
 
   competitions = this.competitionService.getCompetitions();
 
+  createNew = signal<Boolean>(false);
 
+  newComp: Competition = {
+      name: "",
+      date: DateTime.local(),
+      groups: [],
+      unassignedParticipants: []
+    };
+  newDate = DateTime.local().toISODate();
 
+  createNewComp() {
+    if(!this.createNew()) {
+      this.createNew.set(true);
+    }
+  }
 
+  saveComp() {
+    this.competitions = [...this.competitions, this.newComp];
+    this.newComp = {
+      name: "",
+      date: DateTime.local(),
+      groups: [],
+      unassignedParticipants: []
+    };
+    this.createNew.set(false);
+  }
 }
