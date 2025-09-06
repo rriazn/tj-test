5fc261901f943b98ec42a347d892818644775b3f
rriazn <rnriazanov@gmail.com>
2025-09-06 16:04:38 +0200

    manage judges

---
+++ src/app/model/score.type.ts
@@ -0,0 +1,29 @@
+export type Score = {
+    execution1: number,
+    execution2: number,
+    execution3: number,
+    execution: number,
+    execution5: number,
+    execution6: number,
+    difficulty: number,
+    hd: number,
+    tof: number,
+    sync1: number,
+    sync2: number
+}
+
+export function createEmptyScore(): Score {
+  return {
+    execution1: 0,
+    execution2: 0,
+    execution3: 0,
+    execution: 0,
+    execution5: 0,
+    execution6: 0,
+    difficulty: 0,
+    hd: 0,
+    tof: 0,
+    sync1: 0,
+    sync2: 0,
+  };
+}
