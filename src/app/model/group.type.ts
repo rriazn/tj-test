98f7f76662b391a030e0a6628c723989f42bf130
rriazn <rnriazanov@gmail.com>
2025-09-08 11:15:14 +0200

    participant scores

--- src/app/model/group.type.ts
+++ src/app/model/group.type.ts
@@ -1,7 +1,8 @@
 import { Participant } from "./participant.type";
+import { Score } from "./score.type";
 
 
 export type Group = {
     title: string,
-    participants: Participant[]
+    participants: [Participant, Score][]
 }
